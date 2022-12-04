5003f604bc064a917633bc4e7f09d040e237b371
DeeptiHari21 <107431069+DeeptiHari21@users.noreply.github.com>
2022-12-04 11:54:09 +0530

    Update car_parking.py

--- car_parking.py
+++ car_parking.py
@@ -2,8 +2,7 @@ import json
 
 
 class parking_lot:
-    """Creates a virtual parking lot based on input size of the lot:
-    Create a parking lot class that takes in a square footage size as input and creates an array of empty values based on the input square footage size. Assume every parking spot is 8x12 (96 ft2) for this program, but have the algorithm that calculates the array size be able to account for different parking spot sizes. For example, a parking lot of size 2000ft2 can fit 20 cars, but if the parking spots were 10x12 (120 ft2), it could only fit 16 cars. The size of the array will determine how many cars can fit in the parking lot. Further create a method for the parking lot class that maps vehicles to parked spots in a JSON object. Call this method at the end of the program, and save the object to a file."""
+    """Creates a virtual parking lot based on input size of the lot."""
 
     def __init__(self):
         """Takes input for the area of the parking lot. Sets the dimensions of the lot to 8x12 if no input is entered."""
@@ -25,10 +24,7 @@ class parking_lot:
 
 
 class license:
-    """Class to take the license plate number and park the car in a spot:
-    Create a car class that takes in a 7 digit license plate and sets it as a property. The car will have 2 methods:
-    1. A magic method to output the license plate when converting the class instance to a string.
-    2. A "park" method that will take a parking lot and spot # as input and fill in the selected spot in the parking lot. If another car is parked in that spot, return a status indicating the car was not parked successfully. If no car is parked in that spot, return a status indicating the car was successfully parked."""
+    """Class to take the license plate number and park the car in a spot."""
 
     def __init__(self, license_number):
         license_number = str(license_number)
@@ -47,7 +43,7 @@ class license:
         else:
             self._license_number = num
 
-    def park(self, lot_number, spot_number, parking_array):
+    def park(self, spot_number, parking_array):
         """Takes in parking lot and spot number and fill it."""
         if spot_number > len(parking_array):
             print("Parking not so big")
@@ -66,8 +62,6 @@ import sys
 
 
 def main():
-    """Main function:
-    Have a main method take an array of cars with random license plates and have them park in a random spot in the parking lot array until the input array is empty or the parking lot is full. If a car tries to park in an occupied spot, have it try to park in a different spot instead until it successfully parks. Once the parking lot is full, exit the program. Output when a car does or does not park successfully to the terminal (Ex. "Car with license plate [LICENSE_PLATE] parked successfully in spot [SPOT #]")."""
     lot = parking_lot()
     parking_spots = lot.spots()
     parked_cars = [None] * len(parking_spots)
@@ -81,7 +75,7 @@ def main():
             spot_number = randint(0, len(parked_cars) - 1)
             if parked_cars[spot_number] == None:
                 car.license_number = car_list[0]
-                car.park(1, spot_number, parked_cars)
+                car.park(spot_number, parked_cars)
                 print(
                     f"Car with license plate number {car.license_number} is parked in spot {spot_number}."
                 )
@@ -95,13 +89,13 @@ def main():
             if len(parking_spots) == 0:
                 if len(car_list) != 0:
                     print("Cars", car_list, "are not parked.")
-                print("The parking order is:", lot.json_map(spots, cars))
-                with open("./parking.json", "w") as outfile:
+                print("The parking is:", lot.json_map(spots, cars))
+                with open("parking.json", "w") as outfile:
                     outfile.write(lot.json_map(spots, cars))
                 sys.exit("Parking lot is full.")
         if len(car_list) == 0:
-            print("The parking order is:", lot.json_map(spots, cars))
-            with open("./parking.json", "w") as outfile:
+            print("The parking is:", lot.json_map(spots, cars))
+            with open("parking.json", "w") as outfile:
                 outfile.write(lot.json_map(spots, cars))
             sys.exit("All cars parked!")
 
